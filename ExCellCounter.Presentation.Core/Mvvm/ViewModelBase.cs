78e89d2419edb6421a378792dbb7ef27d9f74868
pontaGC <makizawawa@gmail.com>
2024-09-08 00:53:36 +0900

    Create ViewModelBase.

---
+++ ExCellCounter.Presentation.Core/Mvvm/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ExCellCounter.Presentation.Core.Mvvm
+{
+    /// <summary>
+    /// The view-model base class.
+    /// </summary>
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
