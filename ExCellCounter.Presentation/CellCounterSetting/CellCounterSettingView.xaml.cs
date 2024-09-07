48fc11f90e120c1c21a1add13f36c5381738b29b
pontaGC <makizawawa@gmail.com>
2024-09-07 20:54:54 +0900

    Create CellCounterSettingView.

---
+++ ExCellCounter.Presentation/CellCounterSetting/CellCounterSettingView.xaml.cs
@@ -0,0 +1,12 @@
+namespace ExCellCounter.Presentation.CellCounterSetting;
+
+/// <summary>
+/// Interaction logic for <see cref="CellCounterSettingView"/>.xaml.
+/// </summary>
+internal partial class CellCounterSettingView
+{
+	public CellCounterSettingView()
+	{
+		InitializeComponent();
+	}
+}
