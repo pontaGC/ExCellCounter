4fec7cda5c1b127d95e66b2abbdcd06c79b7a7e4
pontaGC <makizawawa@gmail.com>
2024-09-08 21:43:44 +0900

    Fixed CommunityToolkit linker problem.

---
+++ ExCellCounter.Presentation/CommunityToolkitInitializer.cs
@@ -0,0 +1,13 @@
+﻿namespace ExCellCounter.Presentation;
+
+internal class CommunityToolkitInitializer
+{
+    static CommunityToolkitInitializer()
+    {
+        // DO NOT REMOVE to resolve CommunityToolkit linker,
+        // unitl the following bug is fixed.
+        // https://github.com/CommunityToolkit/Maui/issues/1218
+        var ignoreMe = typeof(CommunityToolkit.Maui.Views.Expander);
+    }
+};
+
