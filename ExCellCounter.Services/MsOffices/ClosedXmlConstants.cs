b7f4521b111e175af4151c99c3c83ff2e2300a96
pontaGC <makizawawa@gmail.com>
2024-09-29 22:27:22 +0900

    Create ClosedXmlConstants.

---
+++ ExCellCounter.Services/MsOffices/ClosedXmlConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace ExCellCounter.Services.MsOffices
+{
+    /// <summary>
+    /// Constant parameter for ClosedXML.
+    /// </summary>
+    internal static class ClosedXmlConstants
+    {
+        /// <summary>
+        /// Gets a collection of the EXCEL file extension that supports ClosedXML.
+        /// </summary>
+        internal static readonly IReadOnlyCollection<string> SupportedFileExtensions = new[] { ".xlsx", ".xlsm" };
+    }
+}
