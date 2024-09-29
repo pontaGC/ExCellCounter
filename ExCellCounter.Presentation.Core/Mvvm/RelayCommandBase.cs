f392be63907ffa02c98b6cf9d235b4e49c5affdd
pontaGC <makizawawa@gmail.com>
2024-09-29 22:32:40 +0900

    Add RelayCommandBase.

---
+++ ExCellCounter.Presentation.Core/Mvvm/RelayCommandBase.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ExCellCounter.Presentation.Core.Mvvm
+{
+    /// <summary>
+    /// The relay command base.
+    /// </summary>
+    public abstract class RelayCommandBase : IRelayCommand
+    {
+        #region Fields
+
+        private event EventHandler? internalCanExecuteChanged;
+
+        #endregion
+
+        #region Explict interface implementation
+
+        event EventHandler? ICommand.CanExecuteChanged
+        {
+            add => this.internalCanExecuteChanged += value;
+            remove => this.internalCanExecuteChanged -= value;
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return this.CanExecute();
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            this.Execute();
+        }
+
+        void IRelayCommand.NotifyCanExecuteChanged()
+        {
+            this.internalCanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Determines whether the command can execute in its current state.
+        /// </summary>
+        /// <returns><c>true</c> if this command can be executed, otherwise, <c>false</c>.</returns>
+        protected abstract bool CanExecute();
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        protected abstract void Execute();
+
+        #endregion
+    }
+}
---
+++ ExCellCounter.Presentation.Core/Mvvm/RelayCommandBase{T}.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ExCellCounter.Presentation.Core.Mvvm
+{
+    /// <summary>
+    /// The relay command base.
+    /// </summary>
+    /// <typeparam name="T">The type of command parameter.</typeparam>
+    public abstract class RelayCommandBase<T> : IRelayCommand
+    {
+        #region Fields
+
+        private event EventHandler? internalCanExecuteChanged;
+
+        #endregion
+
+        #region Explict interface implementation
+
+        event EventHandler? ICommand.CanExecuteChanged
+        {
+            add => this.internalCanExecuteChanged += value;
+            remove => this.internalCanExecuteChanged -= value;
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            return this.CanExecute((T?)parameter);
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            this.Execute((T?)parameter);
+        }
+
+        void IRelayCommand.NotifyCanExecuteChanged()
+        {
+            this.internalCanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        /// <summary>
+        /// Determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">The command parameter. Can be <c>null</c>.</param>
+        /// <returns><c>true</c> if this command can be executed, otherwise, <c>false</c>.</returns>
+        protected abstract bool CanExecute(T? parameter);
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <param name="parameter">The command parameter. Can be <c>null</c>.</param>
+        protected abstract void Execute(T? parameter);
+
+        #endregion
+    }
+}
