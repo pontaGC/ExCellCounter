2e155f94e121821fe7c4ad99b74fbb6dfc7ca6a3
pontaGC <makizawawa@gmail.com>
2024-09-30 18:23:43 +0900

    Add DataGrid to CellCounterSetting (Implementation has been not completed).

--- ExCellCounter.Presentation/CellCounterSetting/CellCounterSettingViewModel.cs
+++ ExCellCounter.Presentation/CellCounterSetting/CellCounterSettingViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ExCellCounter.Presentation.Core.Mvvm;
+using ExCellCounter.Shared.Rules;
 
 namespace ExCellCounter.Presentation.CellCounterSetting;
 
@@ -9,6 +13,12 @@ namespace ExCellCounter.Presentation.CellCounterSetting;
 /// </summary>
 internal class CellCounterSettingViewModel : ViewModelBase
 {
+    #region Fields
+
+    private WorksheetCellViewModel selectedCellItem;
+
+    #endregion
+
     #region Constructors
 
     /// <summary>
@@ -22,12 +32,33 @@ internal class CellCounterSettingViewModel : ViewModelBase
         };
         var dummyCellAddress = new CellViewModel()
         {
-            Row = "A",
-            Column = "1",
+            CellAddress = "A1",
             Name = "TopCell"
         };
         dummySheet.Cells.Add(dummyCellAddress);
         this.Worksheets.Add(dummySheet);
+
+        var dummySheetCell1 = new WorksheetCellViewModel()
+        {
+            CellAddress = "A1",
+            CellLabel = "English",
+        };
+        dummySheetCell1.SheetName.Value = "Sheet1";
+        var dummySheetCell2 = new WorksheetCellViewModel()
+        {
+            CellAddress = "AZ100",
+            CellLabel = "Math",
+        };
+        dummySheetCell2.SheetName.Value = "Sheet2";
+        this.WorksheetCells.Add(dummySheetCell1);
+        this.WorksheetCells.Add(dummySheetCell2);
+
+        var requiredRule = new DelegateRule<string>(
+            "RequiredSheetName",
+            x => !string.IsNullOrWhiteSpace(x),
+            x => "string.IsNullOrWhiteSpace(x)");
+        dummySheetCell1.SheetName.AddRule(requiredRule);
+        dummySheetCell2.SheetName.AddRule(requiredRule);
     }
 
     #endregion
@@ -38,6 +69,20 @@ internal class CellCounterSettingViewModel : ViewModelBase
     /// Gets a collection of the worksheet which has cells to be counted.
     /// </summary>
     public ObservableCollection<WorksheetViewModel> Worksheets { get; } = new ObservableCollection<WorksheetViewModel>();
+
+    /// <summary>
+    /// Gets or sets the selected cell item.
+    /// </summary>
+    public WorksheetCellViewModel SelectedCellItem
+    {
+        get => this.selectedCellItem;
+        set => this.SetProperty(ref this.selectedCellItem, value);
+    }
+
+    /// <summary>
+    /// Gets a collection of the worksheet and cell to be counted.
+    /// </summary>
+    public ObservableCollection<WorksheetCellViewModel> WorksheetCells { get; } = new ObservableCollection<WorksheetCellViewModel>();
 
     #endregion
 }
---
+++ ExCellCounter.Presentation/CellCounterSetting/WorksheetCellViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ExCellCounter.Presentation.Core.Mvvm;
+
+namespace ExCellCounter.Presentation.CellCounterSetting;
+
+/// <summary>
+/// The view-model for displaying the relationship between worksheets and cells in a flat manner.
+/// </summary>
+internal class WorksheetCellViewModel : ViewModelBase
+{
+    #region Fields
+
+    private string cellAddress = string.Empty;
+    private string cellLabel = string.Empty;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets a name of worksheet.
+    /// </summary>
+    public ValidatableObserveProperty<string> SheetName { get; } = new ValidatableObserveProperty<string>();
+
+    /// <summary>
+    /// Gets or sets a cell address (e.g. AZ10, D2).
+    /// </summary>
+    public string CellAddress
+    {
+        get => this.cellAddress;
+        set => this.SetProperty(ref this.cellAddress, value ?? string.Empty);
+    }
+
+    /// <summary>
+    /// Gets or sets a label of the cell.
+    /// </summary>
+    public string CellLabel
+    {
+        get => this.cellLabel;
+        set => this.SetProperty(ref this.cellLabel, value ?? string.Empty);
+    }
+
+    #endregion
+}
